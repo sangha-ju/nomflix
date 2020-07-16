c0cfc0c2141cf7d629431a7f7c664518b4ab30b7
sangha-ju <48509329+sangha-ju@users.noreply.github.com>
2020-07-16 20:27:54 +0900

    add detail api

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -14,6 +14,8 @@ export default () => (
                 <Route path="/" exact component={Home} />
                 <Route path="/tv" exact component={TV} />
                 <Route path="/search" exact component={Search} />
+                <Route path="/movie/:id" exact component={Detail} />
+                <Route path="/show/:id" exact component={Detail} />
                 <Redirect from="*" to="/" />
             </Switch>
         </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SearchPresenter from "./SearchPresenter";
+import { tvApi } from "../../api";
 
 export default class extends Component {
     state = {
@@ -9,6 +10,36 @@ export default class extends Component {
         loading: false,
         error: null
     };
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if(searchTerm !== "") {
+            this.searchByTerm();
+        }
+    }
+
+    searchByTerm = async () => {
+        const { searchTerm } = this.state;
+        this.setState({ loading: true });
+        try {
+            const { data: { results: movieResults } } = await tvApi.search(searchTerm);
+            const { data: { results: tvResults } } = await tvApi.search(searchTerm);
+            this.setState({
+                movieResults,
+                tvResults
+            })
+        } catch {
+            this.setState({
+                error: "Can't not find results."
+            })
+        } finally {
+            this.setState({
+                loading: false
+            })
+        }
+    }
+
+    
 
     render() {
         const { movieResults, tvResults, searchTerm, loading, error } = this.state;
