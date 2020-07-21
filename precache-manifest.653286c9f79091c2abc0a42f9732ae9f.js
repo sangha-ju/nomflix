56ad32d3214e35d452b3be0da040c005327e9dc9
sangha-ju <48509329+sangha-ju@users.noreply.github.com>
2020-07-21 16:18:30 +0900

    Updates

---
+++ precache-manifest.653286c9f79091c2abc0a42f9732ae9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80970b6d463296e7260f5ceeabb35795",
+    "url": "/JooFlix/index.html"
+  },
+  {
+    "revision": "444db9cbbaefca16547d",
+    "url": "/JooFlix/static/js/2.4e193754.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/JooFlix/static/js/2.4e193754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262d9b22a26529e4ea04",
+    "url": "/JooFlix/static/js/main.8620ca96.chunk.js"
+  },
+  {
+    "revision": "71900a321d1a2a6d2b58",
+    "url": "/JooFlix/static/js/runtime-main.a5dbda6d.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/JooFlix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
